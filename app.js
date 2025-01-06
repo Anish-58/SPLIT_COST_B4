d36c3a68977eb4744cbb469f52c3cbd249f46526
Anish-58 <bouchastha58@gmail.com>
2025-01-06 18:52:01 +1100

    added  1st function

---
+++ app.js
@@ -0,0 +1,6 @@
+let unsettledAmount = 60;
+function showUnsettledAmount(unsettledAmount) {
+  const unsettledAmountElement = document.getElementById('unsettled_amount');
+  unsettledAmountElement.innerHTML = unsettledAmount;
+}
+showUnsettledAmount(120);
